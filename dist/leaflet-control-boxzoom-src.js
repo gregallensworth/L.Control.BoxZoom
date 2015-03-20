4b8fbf58d3a56986f439c122e65c316c7879cadf
Greg Allensworth <greg.allensworth@gmail.com>
2015-03-19 20:00:23 -0700

    First cut of L.Control.BoxZoom, demo, etc.

---
+++ dist/leaflet-control-boxzoom-src.js
@@ -0,0 +1,67 @@
+/*
+ * L.Control.BoxZoom
+ * A visible, clickable control for doing a box zoom.
+ * https://github.com/gregallensworth/L.Control.BoxZoom
+ */
+L.Control.BoxZoom = L.Control.extend({
+    options: {
+        position: 'topright'
+    },
+    initialize: function(options) {
+        L.setOptions(this, options);
+        this.map    = null;
+        this.active = false;
+    },
+    onAdd: function (map) {
+        // add a linkage to the map, since we'll be managing map layers
+        this.map = map;
+        this.active = false;
+
+        // create our button: uses FontAwesome cuz that font is... awesome
+        // assign this here control as a property of the visible DIV, so we can be more terse when writing click-handlers on that visible DIV
+        this.controlDiv           = L.DomUtil.create('div', 'leaflet-control-boxzoom');
+        this.controlDiv.control   = this;
+        this.controlDiv.title     = 'Click here then draw a square on the map, to zoom in to an area';
+        this.controlDiv.innerHTML = '<i class="fa fa-search-plus leaflet-control"></i>';
+        L.DomEvent
+            .addListener(this.controlDiv, 'mousedown', L.DomEvent.stopPropagation)
+            .addListener(this.controlDiv, 'click', L.DomEvent.stopPropagation)
+            .addListener(this.controlDiv, 'click', L.DomEvent.preventDefault)
+            .addListener(this.controlDiv, 'click', function () {
+                this.control.toggleState();
+            });
+
+        // start by toggling our state to off; this disables the boxZoom hooks on the map, in favor of this one
+        this.setStateOff();
+
+        // done!
+        return this.controlDiv;
+    },
+
+    toggleState: function () {
+        this.active ? this.setStateOff() : this.setStateOn();
+    },
+    setStateOn: function () {
+        L.DomUtil.addClass(this.controlDiv,'leaflet-control-boxzoom-active');
+        this.active = true;
+        this.map.dragging.disable();
+        this.map.boxZoom.addHooks();
+
+        this.map.on('mousedown', this.handleMouseDown, this);
+        this.map.on('boxzoomend', this.setStateOff, this);
+    },
+    setStateOff: function () {
+        L.DomUtil.removeClass(this.controlDiv,'leaflet-control-boxzoom-active');
+        this.active = false;
+        this.map.off('mousedown', this.handleMouseDown, this);
+        this.map.dragging.enable();
+        this.map.boxZoom.removeHooks();
+    },
+
+    handleMouseDown: function (event) {
+        this.map.boxZoom._onMouseDown.call(this.map.boxZoom, { clientX:event.originalEvent.clientX, clientY:event.originalEvent.clientY, which:1, shiftKey:true });
+    }
+});
+L.Control.boxzoom = function(options) {
+    return new L.Control.BoxZoom(options);
+}
